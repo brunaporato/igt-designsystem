c385ec4c17e35d6395e34a1aca4618be864022fe
Bruna <brunaporato@gmail.com>
2023-12-13 08:39:17 -0300

    add text component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quidem nemo error obcaecati culpa, voluptas repudiandae, deserunt voluptates eius assumenda totam dolorum iste doloremque magnam. In atque odio debitis beatae?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
