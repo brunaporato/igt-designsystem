3f8028b59684cd0bdc61758dfd667af3e124475c
Bruna <brunaporato@gmail.com>
2023-12-13 15:05:58 -0300

    fix options for storybook

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/brunaporato.png',
     alt: 'Bruna Porato',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
